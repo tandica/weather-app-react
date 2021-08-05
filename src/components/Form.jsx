1f1155aba493f0517bc6596387242e97b366ac86
tandica <williams.tandica@gmail.com>
2021-08-05 15:49:03 -0400

    render temp, min, max and humidity on page

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,10 @@
 import { useState, useEffect } from "react";
-import WeatherList from "./WeatherList";
 import Weather from "./Weather";
-//import axios from "axios";
 require("dotenv").config();
 
 export default function Form(props) {
   const [input, setInput] = useState("");
-  const [cityID, setCityID] = useState("");
-  const [state, setState] = useState("");
-  const [city, setCity] = useState("");
-  const [weatherData, setWeatherData] = useState({});
   const [forecast, setForecast] = useState([]);
-  const [location, setLocation] = useState("");
 
   async function getWeatherData(e) {
     e.preventDefault();
@@ -23,26 +16,16 @@ export default function Form(props) {
       .then((res) => res.json())
       .then((data) => {
         const weatherInfo = [...new Set(data.list.map((item) => item))];
-        const getID = data.city.id;
-        //setForecast({ data: data });
-        console.log("getid", getID);
-        setCityID(getID);
+
         console.log("weatherinfoooo", weatherInfo);
         console.log("weatherinfoooo2", weatherInfo[0]);
         setForecast(weatherInfo);
       });
-    //.then((data) => data);
-    //console.log("newnew", data[0].main);
-    // console.log("temp ?????", data[0].main.temp);
-    //console.log("name of city", data.name);
-    //console.log("forecast2", forecast.data);
-    //setForecast({ data: data });
-    //console.log("setforecast ###", setForecast());
+
     console.log("what is data", data);
   }
 
   console.log("forecast checkkk", forecast[0]);
-  console.log("city id checkkkkk", cityID);
 
   // const currentTemp = [...new Set(forecast.map((item) => item.main))];
   // console.log("CURRENT TEMP ^^^^", currentTemp);
@@ -87,10 +70,6 @@ export default function Form(props) {
           Submit
         </button>
       </form>
-      {/* <div>
-        <WeatherList data={forecast.data} />
-      </div> */}
-
       <div className="weather">
         <span>{displayWeather()}</span>
       </div>
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,11 +1,18 @@
 export default function Weather(props) {
   console.log("weather props", props);
-  console.log("weather props temp ====>", props.temp);
+  console.log("weather props temp ====>", props.item.temp);
+
   return (
     <div className="card">
       <div className="card-body">
-        <p className="card-text">{props.temp}</p>
-        <p>test</p>
+        <p className="card-text">{Math.round(props.item.temp - 273.15)}°C</p>
+        <p className="card-text">
+          High: {Math.round(props.item.temp_max - 273.15)}°C
+        </p>
+        <p className="card-text">
+          Low: {Math.round(props.item.temp_min - 273.15)}°C
+        </p>
+        <p className="card-text">Humidity: {props.item.humidity}%</p>
       </div>
     </div>
   );
