909b2aff8873c058fc632fde68ce2e390dc591a2
tandica <williams.tandica@gmail.com>
2021-08-06 02:51:20 -0400

    update bg image and change font and weather card colour

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -164,7 +164,7 @@ export default function Form(props) {
         </div>
         <button
           type="submit"
-          className="btn btn-primary"
+          className="btn btn-light"
           onClick={(e) => {
             getWeatherData(e);
             getWeatherDataZip(e);
